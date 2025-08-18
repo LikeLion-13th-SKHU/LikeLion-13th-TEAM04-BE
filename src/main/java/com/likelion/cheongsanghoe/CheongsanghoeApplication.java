1ab148322c7426e17d5a2821ebdce44a68a5daad
daun <wjdekdns0218@gmail.com>
2025-08-19 02:01:14 +0900

    [FIX/#17] SwaggerConfig 추가

--- src/main/java/com/likelion/cheongsanghoe/CheongsanghoeApplication.java
+++ src/main/java/com/likelion/cheongsanghoe/CheongsanghoeApplication.java
@@ -2,10 +2,12 @@ package com.likelion.cheongsanghoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CheongsanghoeApplication {
 
     public static void main(String[] args) {
